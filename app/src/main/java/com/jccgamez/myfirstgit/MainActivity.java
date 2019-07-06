d05d50819a911335c1a17cac5bc33143898fc943
Roberto <Roberto>
2019-07-06 11:34:20 -0500

    Se agrego codigo de botones

--- app/src/main/java/com/jccgamez/myfirstgit/MainActivity.java
+++ app/src/main/java/com/jccgamez/myfirstgit/MainActivity.java
@@ -2,7 +2,12 @@ package com.jccgamez.myfirstgit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +18,15 @@ public class MainActivity extends AppCompatActivity {
         // Comentario solo para cambiar el archivo
         //Hola
 
+        EditText txtUsuario = findViewById(R.id.txtUsuario);
+        EditText txtPassword = findViewById(R.id.txtPassword);
+        Button btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getBaseContext(),"hola", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
