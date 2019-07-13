be3c2c008d67ca9a6c79358b3cbe8cd2c1ee37f7
Manuel Salas <alejandro@locreas.com>
2019-07-13 12:06:56 -0500

    post request

--- app/src/main/java/com/jccgamez/myfirstgit/MainActivity.java
+++ app/src/main/java/com/jccgamez/myfirstgit/MainActivity.java
@@ -3,11 +3,24 @@ package com.jccgamez.myfirstgit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,6 +42,15 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Toast.makeText(getBaseContext(),"hola", Toast.LENGTH_LONG).show();
+                try {
+                    //new HTTPAsyncTask().HttpPost("http://locreas.com/ws/login.php");
+                    PeticionAsincrona  peticion = new PeticionAsincrona();
+                    peticion.HttpPost("http://locreas.com/ws/login.php");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
         });
 
@@ -47,5 +69,58 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(abrirSensor);
             }
         });
+    }
+
+    private class PeticionAsincrona extends AsyncTask<String, Void, String> {
+        private String HttpPost(String myUrl) throws IOException, JSONException {
+            String result = "";
+            URL url = new URL(myUrl);
+            // HttpURLConnection es la clase con la que hacemos un request a un webservice
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/json; charset=utf-8");
+
+            // Creamos objeto json y le agregamos los valores de lass cajas de texto
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.accumulate("usuario", "fauno");
+            jsonObject.accumulate("password", "fauno*");
+
+            OutputStream os = conn.getOutputStream();
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, StandardCharsets.UTF_8));
+            writer.write(jsonObject.toString());
+            System.out.println("MS " + jsonObject.toString());
+            Log.i(MainActivity.class.toString(), jsonObject.toString());
+            writer.flush();
+            writer.close();
+            os.close();
+
+            conn.connect();
+
+            // 5. return response message
+            return conn.getResponseMessage()+"";
+
+        }
+
+
+
+        @Override
+        protected String doInBackground(String... urls) {
+            // params comes from the execute() call: params[0] is the url.
+            try {
+                try {
+                    return HttpPost(urls[0]);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    return "Error!";
+                }
+            } catch (IOException e) {
+                return "Unable to retrieve web page. URL may be invalid.";
+            }
+        }
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+
+        }
     }
 }
